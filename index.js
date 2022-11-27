8ed460a26fd98edb6fa5e80b75093bbeffa3d061
Helena Tenev <htenev14@gmail.com>
2022-11-26 22:09:16 -0500

    commit tree 10857fbaf98e265de6a4e2da5534081d960136f4

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (names) {
+    // while (names.length > 2) {
+    //     names.pop();
+    // }
+    // return names;
+    return names.slice(0, 2);
+};
+
+
+
+const returnLastTwoDrivers = function (names) {
+    return names.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return function mult(fare) {
+        return int * fare;
+    }   
+};
+
+function fareDoubler(fare) {
+    return 2 * fare;
+    };
+
+function fareTripler(fare) {
+    return 3 * fare;
+    };    
+
+function selectDifferentDrivers(drivers,returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+};
